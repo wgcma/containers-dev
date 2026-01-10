d118b3c37b10fe9e69c85677ce14c6515672b65d
Wing Ma <wingcheungma@gmail.com>
2026-01-09 16:39:31 -0800

    Add a simple node.js app to print system time in a docker container.

---
+++ nodejs-simple-container/Dockerfile
@@ -0,0 +1,8 @@
+FROM node:20-alpine
+
+WORKDIR /app
+
+COPY package.json ./
+COPY index.js ./
+
+CMD ["node", "index.js"]
---
+++ nodejs-simple-container/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+console.log('System Time:', new Date().toISOString());
+console.log('Local Time:', new Date().toString());
+console.log('Unix Timestamp:', Date.now());
